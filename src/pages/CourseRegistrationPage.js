6fd6cf85af5260e09d1e6dba96bb68ad9532d307
Berke Çelik <berke.celik.zagor@gmail.com>
2023-05-04 22:58:52 +0300

    regPage

--- src/pages/CourseRegistrationPage.js
+++ src/pages/CourseRegistrationPage.js
@@ -1,6 +1,12 @@
-import React from "react";
-import Navbar from "../components/Navbar";
+import React, { useState } from "react";
 import {
+  Container,
+  Grid,
+  Typography,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
   Table,
   TableBody,
   TableCell,
@@ -8,55 +14,319 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
 } from "@mui/material";
+import Navbar from "../components/Navbar";
 
-const CourseRegistrationPage = () => {
-  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-  const hours = [
-    "08:30 - 09:20",
-    "09:30 - 10:20",
-    "10:30 - 11:20",
-    "11:30 - 12:20",
-    "12:30 - 13:20",
-    "13:30 - 14:20",
-    "14:30 - 15:20",
-    "15:30 - 16:20",
-  ];
+const mockCourses = [
+  {
+    id: 1,
+    code: "CMPE101",
+    name: "Logic Design",
+    instructor: "John Doe",
+    periods: [
+      { day: "Monday", startTime: "08:30", endTime: "10:30" },
+      { day: "Wednesday", startTime: "08:30", endTime: "10:30" },
+      { day: "Friday", startTime: "08:30", endTime: "10:30" },
+    ],
+  },
+  {
+    id: 2,
+    code: "CMPE312",
+    name: "Software eng",
+    instructor: "Bodur",
+    periods: [
+      { day: "Monday", startTime: "11:00", endTime: "13:00" },
+      { day: "Wednesday", startTime: "11:00", endTime: "13:00" },
+      { day: "Friday", startTime: "11:00", endTime: "13:00" },
+    ],
+  },
+  {
+    id: 3,
+    code: "CMPE115",
+    name: "Netwrok",
+    instructor: "Arifler",
+    periods: [
+      { day: "Tuesday", startTime: "14:00", endTime: "16:00" },
+      { day: "Thursday", startTime: "14:00", endTime: "16:00" },
+      { day: "Friday", startTime: "14:00", endTime: "16:00" },
+    ],
+  },
+  {
+    id: 4,
+    code: "CMPE301",
+    name: "Highend Embeded",
+    instructor: "John Doe",
+    periods: [
+      { day: "Tuesday", startTime: "16:30", endTime: "18:30" },
+      { day: "Thursday", startTime: "16:30", endTime: "18:30" },
+      { day: "Friday", startTime: "16:30", endTime: "18:30" },
+    ],
+  },
+  {
+    id: 5,
+    code: "CMPE342",
+    name: "Client/Server Prog",
+    instructor: "John Doe",
+    periods: [
+      { day: "Tuesday", startTime: "16:30", endTime: "18:30" },
+      { day: "Wednesday", startTime: "8:30", endTime: "10:30" },
+      { day: "Friday", startTime: "12:30", endTime: "14:30" },
+    ],
+  },
+  {
+    id: 6,
+    code: "CMPE344",
+    name: "Computer Networks",
+    instructor: "John Doe",
+    periods: [
+      { day: "Monday", startTime: "16:30", endTime: "18:30" },
+      { day: "Thursday", startTime: "10:30", endTime: "12:30" },
+      { day: "Friday", startTime: "18:30", endTime: "20:30" },
+    ],
+  }, // Clashing with CMPE101
+  {
+    id: 7,
+    code: "SCIE223",
+    name: "Patterns of Drug Use",
+    instructor: "John Doe",
+    periods: [
+      { day: "Tuesday", startTime: "8:30", endTime: "10:30" },
+      { day: "Thursday", startTime: "14:30", endTime: "16:30" },
+      { day: "Friday", startTime: "12:30", endTime: "14:30" },
+    ],
+  }, // Clashing with CMPE201
+];
+
+function CourseRegistrationPage() {
+  const [selectedCourses, setSelectedCourses] = useState([]);
+  const [warningMessage, setWarningMessage] = useState("");
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [clashingCourse, setClashingCourse] = useState(null);
+  const [courseExistsDialogOpen, setCourseExistsDialogOpen] = useState(false);
+  const [maxCoursesDialogOpen, setMaxCoursesDialogOpen] = useState(false);
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
+    setClashingCourse(null);
+  };
+  const isCourseClashing = (newCourse) => {
+    return selectedCourses.some((selectedCourse) =>
+      selectedCourse.periods.some((selectedPeriod) =>
+        newCourse.periods.some(
+          (newPeriod) =>
+            selectedPeriod.day === newPeriod.day &&
+            ((selectedPeriod.startTime <= newPeriod.startTime &&
+              newPeriod.startTime < selectedPeriod.endTime) ||
+              (newPeriod.startTime <= selectedPeriod.startTime &&
+                selectedPeriod.startTime < newPeriod.endTime))
+        )
+      )
+    );
+  };
+  const handleConfirmClashingCourse = () => {
+    setSelectedCourses([...selectedCourses, clashingCourse]);
+    setDialogOpen(false);
+    setClashingCourse(null);
+  };
+  const isCourseAlreadySelected = (course) => {
+    return selectedCourses.some(
+      (selectedCourse) => selectedCourse.id === course.id
+    );
+  };
+
+  const addCourse = (course) => {
+    if (isCourseAlreadySelected(course)) {
+      setCourseExistsDialogOpen(true);
+      return;
+    }
+    if (selectedCourses.length >= 5) {
+      setMaxCoursesDialogOpen(true);
+      return;
+    }
+
+    if (isCourseClashing(course)) {
+      setClashingCourse(course);
+      setDialogOpen(true);
+    } else {
+      setSelectedCourses([...selectedCourses, course]);
+    }
+  };
+  const removeCourse = (courseId) => {
+    setSelectedCourses(
+      selectedCourses.filter((course) => course.id !== courseId)
+    );
+  };
 
   return (
     <div>
       <Navbar />
-      <h1>Course Registration</h1>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell></TableCell>
-              {days.map((day) => (
-                <TableCell key={day}>{day}</TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {hours.map((hour, index) => (
-              <TableRow key={hour}>
-                <TableCell>{hour}</TableCell>
-                {[...Array(days.length)].map((_, index) => (
-                  <TableCell
-                    key={`${hour}-${index}`}
-                    style={{
-                      backgroundColor: "lightblue",
-                      border: "1px solid lightblue",
-                    }}
-                  ></TableCell>
-                ))}
+      <Container maxWidth="md">
+        <Typography variant="h4" gutterBottom textAlign={"center"}>
+          Avaliable Courses
+        </Typography>
+        <Grid container spacing={2}>
+          {mockCourses.map((course) => (
+            <Grid item xs={12} sm={6} md={4} key={course.id}>
+              <Card
+                sx={{
+                  minWidth: 275,
+                  marginBottom: 2,
+                }}
+              >
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    {course.code}
+                  </Typography>
+                  <Typography variant="h5" component="h2">
+                    {course.name}
+                  </Typography>
+                  <Typography variant="h5" component="h2">
+                    {course.instructor}
+                  </Typography>
+                  {course.periods.map((period, index) => (
+                    <Typography
+                      key={index}
+                      variant="body2"
+                      color="textSecondary"
+                      gutterBottom
+                    >
+                      {`${period.day}, ${period.startTime}-${period.endTime}`}
+                    </Typography>
+                  ))}
+                </CardContent>
+                <CardActions>
+                  <Button
+                    size="small"
+                    color="primary"
+                    onClick={() => addCourse(course)}
+                  >
+                    Add
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+        <Typography
+          align={"center"}
+          variant="h5"
+          gutterBottom
+          style={{ marginTop: "20px" }}
+        >
+          Courses Selected
+        </Typography>
+        <TableContainer component={Paper} sx={{ m: 4 }}>
+          <Dialog
+            open={maxCoursesDialogOpen}
+            onClose={() => setMaxCoursesDialogOpen(false)}
+            aria-labelledby="max-courses-dialog-title"
+            aria-describedby="max-courses-dialog-description"
+          >
+            <DialogTitle id="max-courses-dialog-title">Warning</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="max-courses-dialog-description">
+                You can select a maximum of 5 courses.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button
+                onClick={() => setMaxCoursesDialogOpen(false)}
+                color="primary"
+              >
+                Ok
+              </Button>
+            </DialogActions>
+          </Dialog>
+
+          <Dialog
+            open={courseExistsDialogOpen}
+            onClose={() => setCourseExistsDialogOpen(false)}
+            aria-labelledby="course-exists-dialog-title"
+            aria-describedby="course-exists-dialog-description"
+          >
+            <DialogTitle id="course-exists-dialog-title">Warning</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="course-exists-dialog-description">
+                You have already added this course.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button
+                onClick={() => setCourseExistsDialogOpen(false)}
+                color="primary"
+              >
+                Ok
+              </Button>
+            </DialogActions>
+          </Dialog>
+          <Dialog
+            open={dialogOpen}
+            onClose={handleCloseDialog}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              <Typography variant="h6" color="error">
+                Warning
+              </Typography>
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                {`The course ${clashingCourse?.name} has clashing hours. Do you still want to take it?`}
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleCloseDialog} color="error">
+                Cancel
+              </Button>
+              <Button
+                onClick={handleConfirmClashingCourse}
+                color="primary"
+                autoFocus
+              >
+                Confirm
+              </Button>
+            </DialogActions>
+          </Dialog>
+
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Course Code</TableCell>
+                <TableCell>Course Name</TableCell>
+                <TableCell>Instructor</TableCell>
+                <TableCell>Action</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {selectedCourses.map((course) => (
+                <TableRow key={course.id}>
+                  <TableCell component="th" scope="row">
+                    {course.code}
+                  </TableCell>
+                  <TableCell>{course.name}</TableCell>
+                  <TableCell>{course.instructor}</TableCell>
+                  <TableCell>
+                    <Button
+                      size="small"
+                      color="secondary"
+                      onClick={() => removeCourse(course.id)}
+                    >
+                      Remove
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
     </div>
   );
-};
+}
 
 export default CourseRegistrationPage;
